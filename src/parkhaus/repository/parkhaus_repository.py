97c643e5de08b83c2ebbc9af301e8dba40901e67
Jonah Doll <187488776+jonahdoll@users.noreply.github.com>
2026-03-30 15:58:02 +0200

    WIP

---
+++ src/parkhaus/repository/parkhaus_repository.py
@@ -0,0 +1,34 @@
+"""Repository für persistente Parkhaus-Daten."""
+
+from loguru import logger
+from sqlalchemy import select
+from sqlalchemy.orm import Session, joinedload
+from parkhaus.entity.parkhaus import Parkhaus
+from typing import Final
+
+
+class ParkhausRepository:
+    """Repository-Klasse mit CRUD-Methoden für die Entity-Klasse Parkhaus."""
+
+    def find_by_id(self, parkhaus_id: int | None, session: Session) -> Parkhaus | None:
+        """Suche mit der Parkhaus-ID.
+
+        :param parkhaus_id: Die ID des gesuchten Parkhauses.
+        :param session: Session für SQLAlchemy.
+        :return: Das gefundene Parkhaus oder None.
+        :rtype: Parkhaus | None
+        """
+        logger.debug("parkhaus_id={}", parkhaus_id)  # NOSONAR
+
+        if parkhaus_id is None:
+            return None
+
+        statement: Final = (
+            select(Parkhaus)
+            .options(joinedload(Parkhaus.adresse))
+            .where(Parkhaus.id == parkhaus_id)
+        )
+        parkhaus: Final = session.scalar(statement)
+
+        logger.debug("parkhaus={}", parkhaus)
+        return parkhaus
