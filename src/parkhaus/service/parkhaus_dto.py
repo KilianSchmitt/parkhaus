c125ec5c61ff65be6d238cfb1e56c0e4df774a03
Jonah Doll <187488776+jonahdoll@users.noreply.github.com>
2026-03-31 20:22:39 +0200

    Add parkhaus_dto.py

---
+++ src/parkhaus/service/parkhaus_dto.py
@@ -0,0 +1,36 @@
+"""DTO-Klasse für Parkhausdaten."""
+from dataclasses import dataclass
+from decimal import Decimal
+
+import strawberry
+from patient.service.adresse_dto import AdresseDTO
+
+from parkhaus.entity import Parkhaus
+
+__all__ = ["ParkhausDTO"]
+
+
+@dataclass(eq=False, slots=True, kw_only=True)
+@strawberry.type
+class ParkhausDTO:
+    """DTO-Klasse für aus gelesene oder gespeicherte Parkhausdaten: ohne Decorators."""
+
+    id: int
+    version: int
+    name: str
+    kapazitaet: int
+    tarif_pro_stunde: Decimal
+    adresse: AdresseDTO
+
+    def __init__(self, parkhaus: Parkhaus):
+        """Initialisierung von ParkhausDTO durch ein Entity-Objekt von Parkhaus.
+
+        :param parkhaus: Parkhaus-Objekt mit Decorators zu SQLAlchemy
+        """
+        parkhaus_id = parkhaus.id
+        self.id = parkhaus_id if parkhaus_id is not None else -1
+        self.version = parkhaus.version
+        self.name = parkhaus.name
+        self.kapazitaet = parkhaus.kapazitaet
+        self.tarif_pro_stunde = parkhaus.tarif_pro_stunde
+        self.adresse = AdresseDTO(parkhaus.adresse)
