dd4470c88dcb087ecd9f5151562faa5f13820865
Jonah Doll <187488776+jonahdoll@users.noreply.github.com>
2026-03-31 21:34:25 +0200

    Adjust parkhaus_dto

--- src/parkhaus/service/parkhaus_dto.py
+++ src/parkhaus/service/parkhaus_dto.py
@@ -1,11 +1,27 @@
+# Copyright (C) 2023 - present Juergen Zimmermann, Hochschule Karlsruhe
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <https://www.gnu.org/licenses/>.
+
 """DTO-Klasse für Parkhausdaten."""
+
 from dataclasses import dataclass
 from decimal import Decimal
 
 import strawberry
-from patient.service.adresse_dto import AdresseDTO
 
 from parkhaus.entity import Parkhaus
+from parkhaus.service.adresse_dto import AdresseDTO
 
 __all__ = ["ParkhausDTO"]
 
@@ -22,7 +38,7 @@ class ParkhausDTO:
     tarif_pro_stunde: Decimal
     adresse: AdresseDTO
 
-    def __init__(self, parkhaus: Parkhaus):
+    def __init__(self, parkhaus: Parkhaus) -> None:
         """Initialisierung von ParkhausDTO durch ein Entity-Objekt von Parkhaus.
 
         :param parkhaus: Parkhaus-Objekt mit Decorators zu SQLAlchemy
