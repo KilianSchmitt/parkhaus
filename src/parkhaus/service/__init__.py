c6a9a734e2bdf80a3bf4a28e636c738bcd058205
Jonah Doll <187488776+jonahdoll@users.noreply.github.com>
2026-03-31 21:42:14 +0200

    Refactor __init__.py to include missing imports and update __all__ list

--- src/parkhaus/service/__init__.py
+++ src/parkhaus/service/__init__.py
@@ -1,6 +1,13 @@
 """Modul für die Geschäftslogik."""
 
+from parkhaus.service.adresse_dto import AdresseDTO
+from parkhaus.service.exceptions import NotFoundError
+from parkhaus.service.parkhaus_dto import ParkhausDTO
+from parkhaus.service.parkhaus_service import ParkhausService
+
 __all__ = [
-    "ParkhausDTO",
     "AdresseDTO",
+    "NotFoundError",
+    "ParkhausDTO",
+    "ParkhausService",
 ]
