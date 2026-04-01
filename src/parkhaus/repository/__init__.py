7b90ca2dd036a68822322aa2260f54a727a6e410
Jonah Doll <187488776+jonahdoll@users.noreply.github.com>
2026-04-01 18:22:50 +0200

    Fix import typo in __init__.py and add session_factory.py

--- src/parkhaus/repository/__init__.py
+++ src/parkhaus/repository/__init__.py
@@ -2,7 +2,7 @@
 
 from parkhaus.repository.pageable import MAX_PAGE_SIZE, Pageable
 from parkhaus.repository.parkhaus_repository import ParkhausRepository
-from parkhaus.repository.sesssion_factory import Session, engine
+from parkhaus.repository.session_factory import Session, engine
 from parkhaus.repository.slice import Slice
 
 # https://docs.python.org/3/tutorial/modules.html#importing-from-a-package
