0ad0741f553ee8ac0444de7495017fe68aa2e6c2
Jonah Doll <187488776+jonahdoll@users.noreply.github.com>
2026-04-04 16:00:10 +0200

    Add delete_test

---
+++ tests/integration/rest/delete_test.py
@@ -0,0 +1,39 @@
+"""Tests für DELETE."""
+
+from typing import Final
+
+from common_test import ctx, login, rest_url
+from httpx import delete
+from pytest import mark
+
+
+@mark.rest
+@mark.delete_request
+def test_delete() -> None:
+    # arrange
+    patient_id: Final = 5
+
+    # act
+    response: Final = delete(
+        f"{rest_url}/{patient_id}",
+        verify=ctx,
+    )
+
+    # assert
+    assert response.status_code == 204  # noqa: PLR2004
+
+
+@mark.rest
+@mark.delete_request
+def test_delete_not_found() -> None:
+    # arrange
+    patient_id: Final = 999999
+
+    # act
+    response: Final = delete(
+        f"{rest_url}/{patient_id}",
+        verify=ctx,
+    )
+
+    # assert
+    assert response.status_code == 204  # noqa: PLR2004
