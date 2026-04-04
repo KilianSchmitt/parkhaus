e60721537497a68a18d4285d7cd1638912d0b20b
Jonah Doll <187488776+jonahdoll@users.noreply.github.com>
2026-04-04 13:46:08 +0200

    Add parkhaus_write_router with post method.

--- src/parkhaus/router/dependencies.py
+++ src/parkhaus/router/dependencies.py
@@ -6,6 +6,7 @@ from fastapi import Depends
 
 from parkhaus.repository.parkhaus_repository import ParkhausRepository
 from parkhaus.service.parkhaus_service import ParkhausService
+from parkhaus.service.parkhaus_write_service import ParkhausWriteService
 
 
 def get_repository() -> ParkhausRepository:
@@ -22,3 +23,10 @@ def get_service(
 ) -> ParkhausService:
     """Factory-Funktion für ParkhausService."""
     return ParkhausService(repo=repo)
+
+
+def get_write_service(
+    repo: Annotated[ParkhausRepository, Depends(get_repository)],
+) -> ParkhausWriteService:
+    """Factory-Funktion für ParkhausWriteService."""
+    return ParkhausWriteService(repo=repo)
