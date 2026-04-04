8a3196f760c142e063e7f5d4211646ac8e7ce941
KilianSchmitt <116954063+KilianSchmitt@users.noreply.github.com>
2026-04-04 19:33:06 +0200

    Added service find

--- src/parkhaus/service/parkhaus_service.py
+++ src/parkhaus/service/parkhaus_service.py
@@ -7,6 +7,13 @@ from loguru import logger
 from parkhaus.repository import ParkhausRepository, Session
 from parkhaus.service.exceptions import NotFoundError
 from parkhaus.service.parkhaus_dto import ParkhausDTO
+from collections.abc import Mapping
+from parkhaus.repository import (
+    Pageable,
+    ParkhausRepository,
+    Session,
+    Slice,
+)
 
 __all__: list[str] = ["ParkhausService"]
 
@@ -42,3 +49,36 @@ class ParkhausService:
 
         logger.debug("{}", parkhaus_dto)
         return parkhaus_dto
+    
+    def find(
+        self,
+        suchparameter: Mapping[str, str],
+        pageable: Pageable
+    ) -> Slice[ParkhausDTO]:
+        """Findet Parkhäuser anhand von Suchparametern.
+
+        :param suchparameter: Suchparameter
+        :return: Gefundene Parkhäuser als Liste
+        :rtype: Slice[ParkhausDTO]
+        :raises NotFoundError: Wenn kein Parkhaus gefunden wurde.
+        """
+        logger.debug("suchparameter={}, pageable={}", suchparameter, pageable)
+
+        with Session() as session:
+            parkhaus_slice: Final = self.repo.find(
+                suchparameter=suchparameter, pageable=pageable, session=session
+            )
+            if len(parkhaus_slice.content) == 0:
+                raise NotFoundError(suchparameter=suchparameter)
+
+            parkhaus_dto: Final = tuple(
+                ParkhausDTO(parkhaus) for parkhaus in parkhaus_slice.content
+            )
+            session.commit()
+            
+            parkhaus_dto_slice = Slice(
+                content=parkhaus_dto, total_elements=parkhaus_slice.total_elements
+            )
+
+        logger.debug("{}", parkhaus_dto_slice)
+        return parkhaus_dto_slice
