8c72bc24a2e0e321f0a778637a80958adb71837b
Jonah Doll <187488776+jonahdoll@users.noreply.github.com>
2026-04-05 17:55:51 +0200

    Fix codestyle

--- src/parkhaus/repository/parkhaus_repository.py
+++ src/parkhaus/repository/parkhaus_repository.py
@@ -1,11 +1,10 @@
 """Repository für persistente Parkhaus-Daten."""
-from winerror import CO_S_FIRST
-
 from typing import Final
 
 from loguru import logger
 from sqlalchemy import select
 from sqlalchemy.orm import Session, joinedload
+from winerror import CO_S_FIRST
 
 from parkhaus.entity.parkhaus import Parkhaus
 
